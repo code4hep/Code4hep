8cb83c0d940103dd5da2581c14300b8deb430fc5
W. David Dagenhart <wdd@fnal.gov>
2025-11-11 13:30:38 -0600

    Initial implementation of PodioSource

---
+++ IO/plugins/PodioFile.cc
@@ -0,0 +1,141 @@
+#include <cassert>
+#include <exception>
+#include <iomanip>
+
+#include "edm4hep/EventHeaderCollection.h"
+
+#include "PodioFile.h"
+
+#include "DataFormats/Provenance/interface/EventAuxiliary.h"
+#include "DataFormats/Provenance/interface/EventID.h"
+#include "DataFormats/Provenance/interface/HardwareResourcesDescription.h"
+#include "DataFormats/Provenance/interface/ProcessConfiguration.h"
+#include "DataFormats/Provenance/interface/ProcessHistory.h"
+
+#include "FWCore/Framework/interface/EventPrincipal.h"
+#include "FWCore/Framework/interface/LuminosityBlockPrincipal.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/RunPrincipal.h"
+#include "FWCore/Utilities/interface/TimeOfDay.h"
+
+namespace c4h {
+
+  using namespace edm;
+
+  PodioFile::PodioFile(std::string const& fileName, ProcessHistoryRegistry& processHistoryRegistry) {
+    try {
+      podioReader_ = std::make_unique<podio::Reader>(podio::makeReader(fileName));
+      logFileAction("  Successfully opened file ", fileName);
+    } catch (std::exception const& ex) {
+      // Don't print out ex.what() as its info is currently misleading
+      throw Exception(errors::FileOpenError, "PodioSource")
+          << "Failed to open input file \"" << fileName << "\"\nor failed to create podio::Reader\n";
+    }
+    numberOfEvents_ = podioReader_->getEntries("events");
+
+    ParameterSet processParameterSet;
+    processParameterSet.registerIt();
+    ProcessConfiguration processConfiguration("PODIO", processParameterSet.id(), "", HardwareResourcesDescription());
+    ProcessHistory processHistory;
+    processHistory.push_back(processConfiguration);
+    processHistory.setProcessHistoryID();
+    processHistoryRegistry.registerProcessHistory(processHistory);
+    processHistoryID_ = processHistory.id();
+  }
+
+  InputSource::ItemType PodioFile::getNextItemType() {
+    assert(!getNextItemTypeCalled_);
+    getNextItemTypeCalled_ = true;
+
+    if (numberOfEvents_ == 0) {
+      return InputSource::ItemType::IsFile;
+    }
+    if (nextItemType_ == InputSource::ItemType::IsEvent || nextItemType_ == InputSource::ItemType::IsInvalid) {
+      if (nextItemType_ != InputSource::ItemType::IsInvalid) {
+        ++nextEventIndex;
+      }
+
+      // Already processed the last event in the file
+      if (nextEventIndex >= numberOfEvents_) {
+        return InputSource::ItemType::IsFile;
+      }
+
+      frame_ = podioReader_->readFrame("events", nextEventIndex);
+      auto const& eventHeaderCollection = frame_.get<edm4hep::EventHeaderCollection>("EventHeader");
+      auto const& eventHeader = eventHeaderCollection.at(0);
+      RunNumber_t run = eventHeader.getRunNumber();
+      nextEvent_ = eventHeader.getEventNumber();
+      if (nextEvent_ == invalidEventNumber) {
+        throw Exception(errors::LogicError, "PodioSource") << "Invalid event number in file: " << nextEvent_ << "\n";
+      }
+      if (run == invalidRunNumber) {
+        throw Exception(errors::LogicError, "PodioSource") << "Invalid run number in file: " << run << "\n";
+      }
+      if (nextItemType_ == InputSource::ItemType::IsInvalid || run != nextRun_) {
+        nextItemType_ = InputSource::ItemType::IsRun;
+        nextRun_ = run;
+      } else {
+        nextItemType_ = InputSource::ItemType::IsEvent;
+      }
+    } else {
+      if (nextItemType_ == InputSource::ItemType::IsLumi) {
+        nextItemType_ = InputSource::ItemType::IsEvent;
+      } else if (nextItemType_ == InputSource::ItemType::IsRun) {
+        nextItemType_ = InputSource::ItemType::IsLumi;
+      } else {
+        // It should be impossible to get here
+        // There is a bug we need to fix if we do
+        assert(false);
+      }
+    }
+    return nextItemType_;
+  }
+
+  std::shared_ptr<RunAuxiliary> PodioFile::readRunAuxiliary_() {
+    auto runAuxiliary =
+        std::make_shared<RunAuxiliary>(nextRun_, Timestamp::invalidTimestamp(), Timestamp::invalidTimestamp());
+    runAuxiliary->setProcessHistoryID(processHistoryID_);
+    return runAuxiliary;
+  }
+
+  std::shared_ptr<LuminosityBlockAuxiliary> PodioFile::readLuminosityBlockAuxiliary_() {
+    auto lumiAuxiliary = std::make_shared<LuminosityBlockAuxiliary>(
+        nextRun_, nextLumi_, Timestamp::invalidTimestamp(), Timestamp::invalidTimestamp());
+    lumiAuxiliary->setProcessHistoryID(processHistoryID_);
+    return lumiAuxiliary;
+  }
+
+  void PodioFile::readRun_(RunPrincipal& runPrincipal, ProcessHistoryRegistry const& processHistoryRegistry) {
+    assert(getNextItemTypeCalled_);
+    getNextItemTypeCalled_ = false;
+    runPrincipal.fillRunPrincipal(processHistoryRegistry);
+  }
+
+  void PodioFile::readLuminosityBlock_(LuminosityBlockPrincipal& lumiPrincipal,
+                                       ProcessHistoryRegistry const& processHistoryRegistry) {
+    assert(getNextItemTypeCalled_);
+    getNextItemTypeCalled_ = false;
+
+    auto history = processHistoryRegistry.getMapped(lumiPrincipal.aux().processHistoryID());
+    lumiPrincipal.fillLuminosityBlockPrincipal(history);
+  }
+
+  void PodioFile::readEvent_(EventPrincipal& eventPrincipal, ProcessHistoryRegistry const& processHistoryRegistry) {
+    assert(getNextItemTypeCalled_);
+    getNextItemTypeCalled_ = false;
+
+    EventAuxiliary eventAuxiliary;
+    eventAuxiliary.id() = EventID(nextRun_, nextLumi_, nextEvent_);
+    eventAuxiliary.setProcessHistoryID(processHistoryID_);
+
+    auto history = processHistoryRegistry.getMapped(processHistoryID_);
+    eventPrincipal.fillEventPrincipal(eventAuxiliary, history);
+  }
+
+  void PodioFile::logFileAction(char const* msg, std::string const& fileName) const {
+    LogAbsolute("fileAction") << std::setprecision(0) << TimeOfDay() << msg << fileName;
+    FlushMessageLog();
+  }
+
+}  // namespace c4h
---
+++ IO/plugins/PodioSource.cc
@@ -0,0 +1,124 @@
+/**\class c4h::PodioSource
+
+  Description: An InputSource for reading Podio files
+
+  Written to support serial processing of files (1 file open at a time).
+
+*/
+//
+// Author:      W. David Dagenhart
+// Created:     31 October 2025
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "PodioFile.h"
+
+#include "FWCore/Framework/interface/FileBlock.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/InputSource.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+namespace c4h {
+
+  using namespace edm;
+
+  class PodioSource : public InputSource {
+  public:
+    PodioSource(ParameterSet const& pset, InputSourceDescription const& desc);
+
+    static void fillDescriptions(ConfigurationDescriptions& descriptions);
+
+  private:
+    ItemTypeInfo getNextItemType() override;
+    std::shared_ptr<RunAuxiliary> readRunAuxiliary_() override;
+    std::shared_ptr<LuminosityBlockAuxiliary> readLuminosityBlockAuxiliary_() override;
+    std::shared_ptr<FileBlock> readFile_() override;
+    void closeFile_() override;
+    void readRun_(RunPrincipal& runPrincipal) override;
+    void readLuminosityBlock_(LuminosityBlockPrincipal& lumiPrincipal) override;
+    void readEvent_(EventPrincipal& eventPrincipal) override;
+
+    std::vector<std::string> inputFiles_;
+    std::vector<std::string>::const_iterator currentFileIter_;
+    std::vector<std::string>::const_iterator lastFileIter_;
+    bool firstFile_{true};
+    std::unique_ptr<PodioFile> podioFile_;
+  };
+
+  PodioSource::PodioSource(ParameterSet const& pset, InputSourceDescription const& desc)
+      : InputSource(pset, desc),
+        inputFiles_(pset.getUntrackedParameter<std::vector<std::string>>("fileNames")),
+        currentFileIter_(inputFiles_.begin()),
+        lastFileIter_(inputFiles_.end() - 1) {
+    if (processingMode() != RunsLumisAndEvents) {
+      throw Exception(errors::Configuration) << "PodioSource constructor: PodioSource does not support\n"
+                                             << "processing modes other than RunsLumisAndEvents\n";
+    }
+    if (currentFileIter_ != inputFiles_.end()) {
+      podioFile_ = std::make_unique<PodioFile>(*currentFileIter_, processHistoryRegistryForUpdate());
+    }
+  }
+
+  void PodioSource::fillDescriptions(ConfigurationDescriptions& descriptions) {
+    ParameterSetDescription desc;
+    desc.addUntracked<std::vector<std::string>>("fileNames")->setComment("Names of files to be processed.");
+    InputSource::fillDescription(desc);
+    descriptions.add("source", desc);
+  }
+
+  InputSource::ItemTypeInfo PodioSource::getNextItemType() {
+    if (firstFile_) {
+      if (currentFileIter_ == inputFiles_.end()) {
+        return ItemType::IsStop;
+      }
+      return ItemTypeInfo(ItemType::IsFile);
+    }
+    ItemType nextItemType = podioFile_->getNextItemType();
+    if (nextItemType != ItemType::IsFile) {
+      return nextItemType;
+    }
+    if (currentFileIter_ == lastFileIter_) {
+      return ItemTypeInfo(ItemType::IsStop);
+    }
+    return ItemTypeInfo(ItemType::IsFile);
+  }
+
+  std::shared_ptr<RunAuxiliary> PodioSource::readRunAuxiliary_() { return podioFile_->readRunAuxiliary_(); }
+
+  std::shared_ptr<LuminosityBlockAuxiliary> PodioSource::readLuminosityBlockAuxiliary_() {
+    return podioFile_->readLuminosityBlockAuxiliary_();
+  }
+
+  std::shared_ptr<FileBlock> PodioSource::readFile_() {
+    if (firstFile_) {
+      firstFile_ = false;
+    } else {
+      ++currentFileIter_;
+      podioFile_ = std::make_unique<PodioFile>(*currentFileIter_, processHistoryRegistryForUpdate());
+    }
+    return std::make_shared<FileBlock>();
+  }
+
+  void PodioSource::closeFile_() { podioFile_.reset(); }
+
+  void PodioSource::readRun_(RunPrincipal& runPrincipal) {
+    podioFile_->readRun_(runPrincipal, processHistoryRegistry());
+  }
+
+  void PodioSource::readLuminosityBlock_(LuminosityBlockPrincipal& lumiPrincipal) {
+    podioFile_->readLuminosityBlock_(lumiPrincipal, processHistoryRegistry());
+  }
+
+  void PodioSource::readEvent_(EventPrincipal& eventPrincipal) {
+    podioFile_->readEvent_(eventPrincipal, processHistoryRegistry());
+  }
+
+}  // namespace c4h
+using c4h::PodioSource;
+DEFINE_FWK_INPUT_SOURCE(PodioSource);
