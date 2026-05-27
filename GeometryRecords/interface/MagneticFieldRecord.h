8aea392e800b04a9af1e0c545df7d7c0e75c1d60
Kevin Pedro <kpedro88@gmail.com>
2026-05-27 15:54:22 -0500

    remove separate magnetic field record (available as dd4hep::OverlayedField from dd4hep::Detector::field())

--- GeometryRecords/interface/MagneticFieldRecord.h
+++
@@ -1,7 +0,0 @@
-#ifndef Code4hep_GeometryRecords_MagneticFieldRecord_h
-#define Code4hep_GeometryRecords_MagneticFieldRecord_h
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class MagneticFieldRecord : public edm::eventsetup::EventSetupRecordImplementation<MagneticFieldRecord> {};
-
-#endif
--- GeometryRecords/src/MagneticFieldRecord.cc
+++
@@ -1,4 +0,0 @@
-#include "Code4hep/GeometryRecords/interface/MagneticFieldRecord.h"
-#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
-
-EVENTSETUP_RECORD_REG(MagneticFieldRecord);
