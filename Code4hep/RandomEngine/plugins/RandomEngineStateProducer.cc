b37e9f555cbc9d02968af222a4058493cc13b479
Soon Yung Jun <soon.yung.jun@cern.ch>
2026-09-17 16:36:43 -0500

    Add an initial random number generator service based on CMSSW IOMC/RandomEngine

---
+++ Code4hep/RandomEngine/plugins/RandomEngineStateProducer.cc
@@ -0,0 +1,41 @@
+
+#include "RandomEngineStateProducer.h"
+
+#include "FWCore/AbstractServices/interface/RandomNumberGenerator.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "Code4hep/RandomEngine/RandomEngineStates.h"
+
+#include <memory>
+
+RandomEngineStateProducer::RandomEngineStateProducer(edm::ParameterSet const&) {
+  produces<edm::RandomEngineStates, edm::Transition::BeginLuminosityBlock>("beginLumi");
+  produces<edm::RandomEngineStates>();
+}
+
+RandomEngineStateProducer::~RandomEngineStateProducer() {}
+
+void RandomEngineStateProducer::produce(edm::StreamID iID, edm::Event& ev, edm::EventSetup const&) const {
+  edm::Service<edm::RandomNumberGenerator>().and_then([&ev](auto const& randomService) {
+    auto states = std::make_unique<edm::RandomEngineStates>();
+    states->setRandomEngineStates(randomService.getEventCache(ev.streamID()));
+    ev.put(std::move(states));
+  });
+}
+
+void RandomEngineStateProducer::globalBeginLuminosityBlockProduce(edm::LuminosityBlock& lb,
+                                                                  edm::EventSetup const&) const {
+  edm::Service<edm::RandomNumberGenerator>().and_then([&lb](auto const& randomService) {
+    auto states = std::make_unique<edm::RandomEngineStates>();
+    states->setRandomEngineStates(randomService.getLumiCache(lb.index()));
+    lb.put(std::move(states), "beginLumi");
+  });
+}
+
+void RandomEngineStateProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  descriptions.add("randomEngineStateProducer", desc);
+}
